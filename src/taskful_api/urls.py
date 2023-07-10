4f7d3adb562441f8a9b449a17f5f6851bfb6f945
Harsh Gor <harshmgor@gmail.com>
2023-07-10 21:31:42 +0530

    Router

--- src/taskful_api/urls.py
+++ src/taskful_api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from users import router as users_api_router
+
+api_url_patterns = [path("accounts/", include(users_api_router.router.urls))]
+
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include(api_url_patterns))]
