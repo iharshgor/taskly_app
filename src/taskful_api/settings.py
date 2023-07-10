281b990d2ef4ab991a03a1da63bde723b280e44b
Harsh Gor <harshmgor@gmail.com>
2023-07-10 22:09:23 +0530

    Permissioins

--- src/taskful_api/settings.py
+++ src/taskful_api/settings.py
@@ -101,6 +101,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FEAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- src/users/viewsets.py
+++ src/users/viewsets.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
+from .permissions import IsUserOwnerOrGetAndPostOnly
 from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsUserOwnerOrGetAndPostOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
